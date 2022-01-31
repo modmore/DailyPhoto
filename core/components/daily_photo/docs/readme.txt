b56bd8a61ecf2cf1fff092a1162f1e5c2a9470b1
Mark Hamstra <hello@markhamstra.com>
2022-01-31 17:46:18 +0100

    Update instructions

--- core/components/daily_photo/docs/readme.txt
+++ core/components/daily_photo/docs/readme.txt
@@ -2,3 +2,11 @@ DailyPhoto
 --------------
 
 Adds a random Unsplash photo to the MODX3 login screen every day.
+
+After installation:
+
+1. Create an application with Unsplash at https://unsplash.com/oauth/applications
+2. Copy/paste the **Access Key** to the `daily_photo.access_key` system setting.
+3. Optionally, configure a custom search query with the `daily_photo.query` system setting that fits with your brand.
+
+The image is cached based on the date, so you'll start seeing a new one after midnight server time.
--- core/components/daily_photo/lexicon/en/default.inc.php
+++ core/components/daily_photo/lexicon/en/default.inc.php
@@ -2,7 +2,7 @@
 
 $_lang['daily_photo'] = 'Daily Photo';
 $_lang['daily_photo.attribution'] = 'Photo by [[+author]] on [[+unsplash]]';
-$_lang['setting_daily_photo.access_key_desc'] = 'The Access Key associated with an application as created at https://unsplash.com/oauth/applications.';
+$_lang['setting_daily_photo.access_key_desc'] = 'The Access Key associated with an application. <a href="https://unsplash.com/oauth/applications" target="_blank" rel="noopener">Create one here</a>.';
 $_lang['setting_daily_photo.access_key'] = 'Application Access Key';
 $_lang['setting_daily_photo.query'] = 'Search Query';
 $_lang['setting_daily_photo.query_desc'] = 'The type of images to show. Use the search on <a href="https://unsplash.com/" target="_blank" rel="noopener">unsplash.com</a> to see what sort of images are returned for a query. To immediately see a new random image, use Manage > Clear Cache after changing this setting to clear the daily image cache. No inspiration? Try "scenic landscape", "creative agency", or "city at night".';
